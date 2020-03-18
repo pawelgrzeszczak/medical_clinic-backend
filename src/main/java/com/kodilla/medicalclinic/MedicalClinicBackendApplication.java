4e792132cf62b1b5d328142ad347e637e35c143e
Pawel <pgrzeszczak@ethernet.pl>
2020-03-18 23:49:25 +0100

    first commit

---
+++ src/main/java/com/kodilla/medicalclinic/MedicalClinicBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.medicalclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalClinicBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicalClinicBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kodilla/medicalclinic/MedicalClinicBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.medicalclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalClinicBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
